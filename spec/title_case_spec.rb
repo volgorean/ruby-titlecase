df26997c9ff498d859cbffa1c18c29fc8db50f3c
Patrick McGreevy & Peter Tinti <student@epicodus.com>
2014-03-03 10:15:24 -0800

    first commit

---
+++ spec/title_case_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('title_case')
+
+describe('title_case') do
+  it('returns a string with proper capitilization when given a sentance') do 
+    title_case('the cake was yummy in my tummy.').should(eq('The Cake was Yummy In My Tummy.'))
+  end
+end
