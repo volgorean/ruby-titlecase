7d9dbdfefaefea83d762fd76524db82ff13282fc
Patrick McGreevy & Peter Tinti <student@epicodus.com>
2014-03-03 10:35:08 -0800

    make title_case leave exception words in lowercase

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,6 +1,35 @@
 
 def title_case(string)
+  exceptions =  ['the', 
+                'a', 
+                'with', 
+                'for', 
+                'on', 
+                'off', 
+                'in', 
+                'of', 
+                'or', 
+                'and', 
+                'but', 
+                'from', 
+                'at', 
+                'to', 
+                'into', 
+                'without', 
+                'within', 
+                'onto', 
+                'upon']
+
   result = ''
-  string.each_line(' ') {|s| result += s.capitalize}
+  split_string = string.split(' ')
+  result += split_string[0].capitalize
+  for i in 1..(split_string.count-1)
+    result += ' '
+    if !exceptions.include?(split_string[i])
+      result += split_string[i].capitalize
+    else
+      result += split_string[i].downcase
+    end
+  end
   result
 end
